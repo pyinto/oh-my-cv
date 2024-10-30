1858bf704b3a12473724053698a6d4da2c9d352e
volodymyr_k <pyinto@gmail.com>
2024-10-30 12:39:39 +0000

    chore: giving more clear naming to dashboard buttons [2]

--- site/src/components/dashboard/File.vue
+++ site/src/components/dashboard/File.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="flex gap-2">
+
+    <!-- Button for exporting as JSON -->
     <UiButton @click="exportToJSON">
       <span i-ic:baseline-save-as size-4 mr-1 />
       {{ $t("dashboard.saveas") }}
     </UiButton>
-    <UiButton
-      class="bg-neutral-800 hover:(bg-neutral-800/90 ring-neutral-800/40) dark:(bg-secondary hover:bg-background hover:ring-secondary/40)"
-      @click="open"
-    >
-      <span i-ic:round-upload-file size-4 mr-1 />
-      {{ $t("dashboard.import") }}
-    </UiButton>
+
+      <!-- Button for importing as JSON -->
+    <UiTooltipProvider :delay-duration="0">
+      <UiTooltip>
+        <UiTooltipTrigger as-child>
+          <UiButton
+            class="bg-neutral-800 hover:(bg-neutral-800/90 ring-neutral-800/40) dark:(bg-secondary hover:bg-background hover:ring-secondary/40)"
+            @click="open"
+          >
+            <span i-ic:round-upload-file size-4 mr-1 />
+            {{ $t("dashboard.import.title") }}
+          </UiButton>
+        </UiTooltipTrigger>
+        <UiTooltipContent side="bottom" class="w-54 p-0 rounded border-destructive/60">
+          <UiAlert variant="destructive" class="border-none rounded-none">
+            <UiAlertTitle>
+              {{ $t("dashboard.import.alert.title") }}
+            </UiAlertTitle>
+            <UiAlertDescription v-html="$t('dashboard.import.alert.content')" />
+          </UiAlert>
+        </UiTooltipContent>
+      </UiTooltip>
+    </UiTooltipProvider>
+
   </div>
 </template>
 
